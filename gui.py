11a4a6fc365cddda0252bcfc16a613b245c29da9
Narsing rao <narsingraogoud32@gmail.com>
2023-08-11 18:58:23 +0530

    create a basic gui without any interactions

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 import PySimpleGUI as sg
 from functions import *
 
-
-
-window = sg.Window("My Ditigal notes ", layout = [""])
+label = sg.Text("Enter Your Note Here")
+inbox = sg.InputText(tooltip="enter note")
+add_button = sg.Button("add")
+window = sg.Window("My Ditigal notes ", layout = [[label] ,[inbox,add_button]])
 window.read()
 window.close()
