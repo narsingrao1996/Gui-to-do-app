7a6db51ab8679d0dfd1902e3e75a06ec00abd982
Narsing rao <narsingraogoud32@gmail.com>
2023-08-14 11:29:42 +0530

    implement add button

--- functions.py
+++ functions.py
@@ -1,7 +1,11 @@
-def read_mode(file_path = "notes.txt"):
+FILE_PATH = "notes.txt"
+
+
+def read_mode(file_path = FILE_PATH):
+
     with open(file_path, "r") as file:
         notes = file.readlines()
     return notes
-def write_mode( note_arg , file_path = "notes.txt"):
+def write_mode( note_arg , file_path = FILE_PATH):
     with open(file_path , "w") as file:
         file.writelines(note_arg)
--- gui.py
+++ gui.py
@@ -1,9 +1,26 @@
-import PySimpleGUI as sg
+import functions
 from functions import *
+import PySimpleGUI as sg
 
 label = sg.Text("Enter Your Note Here")
-inbox = sg.InputText(tooltip="enter note")
+inbox = sg.InputText(tooltip="enter note", key="note")
 add_button = sg.Button("add")
-window = sg.Window("My Ditigal notes ", layout = [[label] ,[inbox,add_button]])
-window.read()
+window = sg.Window("My Ditigal notes ",
+                   layout = [[label] ,[inbox,add_button]] ,
+                   font=('Helvetica', 10))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "add":
+            notes = functions.read_mode()
+            new_notes = values["note"] + "\n"
+            notes.append(new_notes)
+            functions.write_mode(notes)
+        case sg.WINDOW_CLOSED:
+            break
+
+
 window.close()
